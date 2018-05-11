41cb993d8e61155b8570d86e8e9afadaeecbf5de
Dicridon <noahxiong@outlook.com>
2018-05-11 10:19:31 +0800

    10 started

---
+++ 10/10-nat/nat.c
@@ -0,0 +1,114 @@
+#include "nat.h"
+#include "ip.h"
+#include "icmp.h"
+#include "tcp.h"
+#include "rtable.h"
+#include "log.h"
+
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+
+static struct nat_table nat;
+
+// get the interface from iface name
+static iface_info_t *if_name_to_iface(const char *if_name)
+{
+	iface_info_t *iface = NULL;
+	list_for_each_entry(iface, &instance->iface_list, list) {
+		if (strcmp(iface->name, if_name) == 0)
+			return iface;
+	}
+
+	log(ERROR, "Could not find the desired interface according to if_name '%s'", if_name);
+	return NULL;
+}
+
+// determine the direction of the packet, DIR_IN / DIR_OUT / DIR_INVALID
+static int get_packet_direction(char *packet)
+{
+	fprintf(stdout, "TODO: determine the direction of this packet.\n");
+
+	return DIR_INVALID;
+}
+
+// do translation for the packet: replace the ip/port, recalculate ip & tcp
+// checksum, update the statistics of the tcp connection
+void do_translation(iface_info_t *iface, char *packet, int len, int dir)
+{
+	fprintf(stdout, "TODO: do translation for this packet.\n");
+}
+
+void nat_translate_packet(iface_info_t *iface, char *packet, int len)
+{
+	int dir = get_packet_direction(packet);
+	if (dir == DIR_INVALID) {
+		log(ERROR, "invalid packet direction, drop it.");
+		icmp_send_packet(packet, len, ICMP_DEST_UNREACH, ICMP_HOST_UNREACH);
+		free(packet);
+		return ;
+	}
+
+	struct iphdr *ip = packet_to_ip_hdr(packet);
+	if (ip->protocol != IPPROTO_TCP) {
+		log(ERROR, "received non-TCP packet (0x%0hhx), drop it", ip->protocol);
+		free(packet);
+		return ;
+	}
+
+	do_translation(iface, packet, len, dir);
+}
+
+// nat timeout thread: find the finished flows, remove them and free port
+// resource
+void *nat_timeout()
+{
+	while (1) {
+		fprintf(stdout, "TODO: sweep finished flows periodically.\n");
+		sleep(1);
+	}
+
+	return NULL;
+}
+
+// initialize nat table
+void nat_table_init()
+{
+	memset(&nat, 0, sizeof(nat));
+
+	for (int i = 0; i < HASH_8BITS; i++)
+		init_list_head(&nat.nat_mapping_list[i]);
+
+	nat.internal_iface = if_name_to_iface("n1-eth0");
+	nat.external_iface = if_name_to_iface("n1-eth1");
+	if (!nat.internal_iface || !nat.external_iface) {
+		log(ERROR, "Could not find the desired interfaces for nat.");
+		exit(1);
+	}
+
+	memset(nat.assigned_ports, 0, sizeof(nat.assigned_ports));
+
+	pthread_mutex_init(&nat.lock, NULL);
+
+	pthread_create(&nat.thread, NULL, nat_timeout, NULL);
+}
+
+// destroy nat table
+void nat_table_destroy()
+{
+	pthread_mutex_lock(&nat.lock);
+
+	for (int i = 0; i < HASH_8BITS; i++) {
+		struct list_head *head = &nat.nat_mapping_list[i];
+		struct nat_mapping *mapping_entry, *q;
+		list_for_each_entry_safe(mapping_entry, q, head, list) {
+			list_delete_entry(&mapping_entry->list);
+			free(mapping_entry);
+		}
+	}
+
+	pthread_kill(nat.thread, SIGTERM);
+
+	pthread_mutex_unlock(&nat.lock);
+}
